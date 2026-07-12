5851adc1dbaab6fdb3f8f0eee04579f37382f9f6
Soumya Utthasini <60193959+soumyauthhasini@users.noreply.github.com>
2026-07-12 10:19:17 +0530

    Update

--- flexible-content/gallery.php
+++ flexible-content/gallery.php
@@ -3,11 +3,8 @@
         'heading'                 => get_sub_field('heading') ?: '',
         'subheading'              => get_sub_field('subheading') ?: '',
         'description'             => get_sub_field('description') ?: '',
-        'extra_description'       => get_sub_field('extra_description') ?: '',
-        'cards_items'             => get_sub_field('cards_items') ?: '',
         'cta_button_one'          => get_sub_field('cta_button_one') ?: '',
         'cta_button_two'          => get_sub_field('cta_button_two') ?: '',
-        'gallery_images'          => get_sub_field('gallery_images') ?: '',
         'gallery_image'           => get_sub_field('gallery_image') ?: '',
     ];
 
